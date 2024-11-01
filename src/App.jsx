1f64410e61ce229508271160010e120c4c71a224
Smrutishree Samanta <smrutishreesamanta25@gmail.com>
2024-11-01 18:38:16 +0530

    this is second commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+function App()
+{
+const[apples,setApples] = useState(0)
+
+return(
+    <>
+    <h1>Number of Apples:{apples}</h1>
+    {apples === 0 ?
+    (<p>No apple left?</p>
+    ):(<p>Great! you have some apple</p>)
+    }
+    <button style={{color:'red'}} onClick={()=>{
+    setApples(apples+1)
+    }}>Add 1 Apple</button>
+    <button style={{color:'red'}} onClick={()=>apples> 0 && setApples(apples-1)}>remove 1 apple</button>
+    <button style={{color:'red'}} onClick={()=>setApples(0)}>Reset</button>
+    </>
+)
+}
+export default App;
